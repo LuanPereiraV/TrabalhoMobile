23d9441a01aaa9930e2abc2ace833fff401aff43
amandaspolaor <78451480+amandaspolaor@users.noreply.github.com>
2021-08-20 23:46:32 -0300

    tela cadastro de disciplinas(em andamento)

--- mobile_class/lib/main.dart
+++ mobile_class/lib/main.dart
@@ -8,9 +8,99 @@ void main() {
   ));
 }
 
-class Cadastro extends StatelessWidget {
-  TextEditingController nameeController = TextEditingController();
-  TextEditingController passworddController = TextEditingController();
+class CadastroDisciplinas extends StatelessWidget {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController datetimeController = TextEditingController();
+  TextEditingController linkController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.book),
+                onPressed: () { Scaffold.of(context).openDrawer(); },
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
+          ),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Qual é o link?',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Registrar Disciplina',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Disciplina',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: datetimeController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Date e Hora',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    controller: linkController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Link da aula',
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 70,
+                    padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text('Cadastrar'),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(datetimeController.text);
+                        print(linkController.text);
+                        showAlertDialog3(context);
+                      },
+                    )),
+              ],
+            ))
+    );
+  }
+}
+
+class CadastroUsuarios extends StatelessWidget {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   TextEditingController emailController = TextEditingController();
 
   @override
@@ -45,13 +135,13 @@ class Cadastro extends StatelessWidget {
                   alignment: Alignment.center,
                   padding: EdgeInsets.all(10),
                   child: Text(
-                    'Register',
+                    'Registrar Usuário',
                     style: TextStyle(fontSize: 20),
                   )),
               Container(
                 padding: EdgeInsets.all(10),
                 child: TextField(
-                  controller: nameeController,
+                  controller: nameController,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Nome',
@@ -73,7 +163,7 @@ class Cadastro extends StatelessWidget {
                 padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
                 child: TextField(
                   obscureText: true,
-                  controller: passworddController,
+                  controller: passwordController,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Senha',
@@ -98,10 +188,11 @@ class Cadastro extends StatelessWidget {
                     color: Colors.blue,
                     child: Text('Cadastrar'),
                     onPressed: () {
-                      print(nameeController.text);
+                      print(nameController.text);
                       print(emailController.text);
-                      print(passworddController.text);
+                      print(passwordController.text);
                       showAlertDialog2(context);
+                      MaterialPageRoute(builder: (context) => CadastroDisciplinas());
                     },
                   )),
             ],
@@ -111,6 +202,36 @@ class Cadastro extends StatelessWidget {
 }
 
 final String _markdownData = "-This is [Google link](https://www.google.com)";
+
+showAlertDialog3(BuildContext context) {
+
+  // set up the button
+  Widget OK = TextButton(
+    child: Text("OK"),
+    onPressed: () {
+      Navigator.pop(
+        context,
+      );
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Disciplina registrada com sucesso!"),
+    // view
+    actions: [
+      OK,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
 
 showAlertDialog2(BuildContext context) {
 
@@ -264,7 +385,7 @@ class _State extends State<MyApp> {
                     //forgot password screen
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => Cadastro()),
+                      MaterialPageRoute(builder: (context) => CadastroUsuarios()),
                     );
                   },
                   textColor: Colors.blue,
@@ -282,7 +403,7 @@ class _State extends State<MyApp> {
                         print(passwordController.text);
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => Mostrar()),
+                          MaterialPageRoute(builder: (context) => CadastroDisciplinas()),
                         );
                       },
                     )),
