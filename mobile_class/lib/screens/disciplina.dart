6d2e229c294e0209eb794807c929344a77d8ef32
amandaspolaor <78451480+amandaspolaor@users.noreply.github.com>
2021-09-30 22:25:13 -0300

    Update disciplina.dart

--- mobile_class/lib/screens/disciplina.dart
+++ mobile_class/lib/screens/disciplina.dart
@@ -90,14 +90,67 @@ class DetailScreen extends StatelessWidget {
                 );
               },
               icon: Icon(Icons.settings),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomePage(),
+                    ),
+                );
+              },
+              icon: Icon(Icons.timer),
+
             )
           ]
+
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Text(disciplina.description),
       ),
 
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _running = true;
+
+  Stream<String> _clock() async* {
+    // This loop will run forever because _running is always true
+    while (_running) {
+      await Future<void>.delayed(Duration(seconds: 1));
+      DateTime _now = DateTime.now();
+      // This will be displayed on the screen as current time
+      yield "${_now.hour} : ${_now.minute} : ${_now.second}";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: StreamBuilder(
+          stream: _clock(),
+          builder: (context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+            return Text(
+              snapshot.data!,
+              style: TextStyle(fontSize: 50, color: Colors.blue),
+            );
+          },
+        ),
+      ),
     );
   }
 }
